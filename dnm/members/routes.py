bf0b763b4d7e6fa0a1b28cd4cfb83ff18f03533e
jlmitche23 <jacquelinemitchell2019@gmail.com>
2020-02-06 18:09:08 -0800

    Please enter the commit message for your changes. Lines starting

--- dnm/members/routes.py
+++ dnm/members/routes.py
@@ -1,4 +1,15 @@
 from flask import Blueprint
-
+from flask import request
 # Blueprint for members
 members = Blueprint("members", __name__)
+
+@members.route("/members/create")
+def create_member():
+    data = request.get_json()
+    member = Member(member_name=data["member_name"], member_image=data["member_image"], member_description=data["member_description"])
+    db.session.add(member)
+    db.session.commit()
+    return {{"Status" : "Created"}}
+
+@members.route("/members/fetch_members")
+def fetch_members():
---
+++ dnm/models.py
@@ -0,0 +1,7 @@
+from dnm import db
+
+class Members(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    member_name = db.Column(db.String, nullable=True)
+    member_image = db.Column(db.Text, nullable=True)
+    
