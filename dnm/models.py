2d430532f419ef879a411df6ecb5b2c09a802f51
Dustin Cai <dvcai@ucdavis.edu>
2020-02-06 18:13:56 -0800

    first branch init

--- dnm/models.py
+++ dnm/models.py
@@ -4,4 +4,6 @@ class Members(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     member_name = db.Column(db.String, nullable=True)
     member_image = db.Column(db.Text, nullable=True)
+
+class References(db.Model):
     
