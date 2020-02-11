f9712aab56a5b5b4f7029831ec65e00d088e7762
Dustin Cai <dvcai@ucdavis.edu>
2020-02-11 03:25:01 -0800

    Implemented most of create, update, and delete routes for reference model as well as updated references class + merged in master branches on dustin's branch

--- dnm/models.py
+++ dnm/models.py
@@ -3,17 +3,15 @@ from dnm import db
 class Member(db.Model):
     #local ID
     id = db.Column(db.Integer, primary_key=True)
-<<<<<<< HEAD
-    member_name = db.Column(db.String, nullable=True)
-    member_image = db.Column(db.Text, nullable=True)
-
-class References(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=True)
-    link = db.Column(db.Text, nullable=True)
-=======
     #ID from strapi
     content_id = db.Column(db.Integer, nullable=True)
     name = db.Column(db.String, nullable=True)
->>>>>>> facd933977807a82fcc9a166e89588161c0f1a96
     image = db.Column(db.Text, nullable=True)
+
+class References(db.Model):
+    #local ID
+    id = db.Column(db.Integer, primary_key=True)
+    #ID from strapi
+    content_id = db.Column(db.Integer, nullable=True)
+    name = db.Column(db.String, nullable=True)
+    link = db.Column(db.Text, nullable=True)
