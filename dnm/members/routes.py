ba67bd5b34ebd429a5fd727e0441422615756209
jlmitche23 <jacquelinemitchell2019@gmail.com>
2020-02-10 18:26:09 -0800

    Created Create Member Route

--- dnm/members/routes.py
+++ dnm/members/routes.py
@@ -1,18 +1,21 @@
 from flask import Blueprint, request
 from dnm import db
+from dnm.models import Member
 
 # Blueprint for members
 members = Blueprint("members", __name__)
 
-@members.route("/members/create")
+@members.route("/members/create", methods=["POST"])
 def create_member():
     data = request.get_json()
-    member = Member(name=data["member_name"], image=data["member_image"])
+    image = "https://include-dnm.herokuapp.com/" + data["entry"]["Image"][0]["url"]
+    member = Member(name=data["entry"]["Name"], image=image, content_id=data["entry"]["id"])
     db.session.add(member)
     db.session.commit()
+    print(data)
     return {"Message" : "Member Created"}
 
-@members.route("/members/edit")
+@members.route("/members/edit", methods=["PUT"])
 def update_members():
     data = request.get_json()
     #might change
@@ -22,7 +25,7 @@ def update_members():
     db.commit()
     return {"Message" : "Member Updated"}
 
-@members.route("/members/delete")
+@members.route("/members/delete", methods=["DELETE"])
 def delete_member():
     data = request.get_json()
     member = Member.query.filter_by(content_id=data["id"]).first()
--- dnm/models.py
+++ dnm/models.py
@@ -1,6 +1,9 @@
 from dnm import db
 
-class Members(db.Model):
+class Member(db.Model):
+    #local ID
     id = db.Column(db.Integer, primary_key=True)
+    #ID from strapi
+    content_id = db.Column(db.Integer, nullable=True)
     name = db.Column(db.String, nullable=True)
     image = db.Column(db.Text, nullable=True)
