176c626e35af67ba76c82ed930843d5839dabb11
Dustin Cai <dvcai@ucdavis.edu>
2020-02-10 16:37:36 -0800

    started delete and update

--- dnm/references/routes.py
+++ dnm/references/routes.py
@@ -3,18 +3,25 @@ from flask import Blueprint, request
 # Blueprint for references
 references = Blueprint("references", __name__)
 
-@references.route("references/create")
+@references.route("/references/create", methods=['GET', 'POST'])
 def create_reference():
-    data = request.get_json()
-    reference = Reference(name=data["name"], link=data["link"], image=data["image"])
+    data=request.get_json()
+    reference=Reference(name=data["name"], link=data["link"], image=data["image"])
     db.session.add(reference)
     db.session.commit()
     return {"Status" : "Reference created"}
 
-# @references.route("references/update")
-# def update_reference():
-#     pass
-#
-# @references.route("references/delete")
-# def delete_reference():
-#     pass
+@references.route("/references/update", methods=['GET', 'POST'])
+def update_reference():
+    data = request.get_json()
+    reference = reference.query.filter_by().first()
+    db.session.commit()
+    return {"Message" : "Reference updated"}
+
+@references.route("references/delete", methods=['GET', 'POST'])
+def delete_reference():
+    data = request.get_json()
+
+    db.session.commit()
+    return {"Status" : "Reference deleted"}
+    pass
