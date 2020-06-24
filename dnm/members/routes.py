db5a854c1f948769d137fd2596d642f0914347ce
wongband <bmwong@ucdavis.edu>
2020-06-24 15:01:30 -0700

    Updated route

--- dnm/members/routes.py
+++ dnm/members/routes.py
@@ -9,7 +9,7 @@ members = Blueprint("members", __name__)
 @members.route("/members/create", methods=["POST"])
 def create_member():
     data = request.get_json()
-    image = "https://include-dnm.herokuapp.com" + data["entry"]["Image"][0]["url"]
+    image = "https://dnm-cms.herokuapp.com" + data["entry"]["Image"][0]["url"]
     member = Member(name=data["entry"]["Name"], image=image, content_id=data["entry"]["id"])
     db.session.add(member)
     db.session.commit()
@@ -23,7 +23,7 @@ def update_members():
     print(data)
     member = Member.query.filter_by(content_id=data["entry"]["id"]).first()
     member.name = data["entry"]["Name"]
-    image_new = "https://include-dnm.herokuapp.com" + data["entry"]["Image"][0]["url"]
+    image_new = "https://dnm-cms.herokuapp.com" + data["entry"]["Image"][0]["url"]
     member.image = image_new
     db.session.commit()
 
